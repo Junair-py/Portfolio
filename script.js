1fd950dc566ed1afe1c375a5fc87ba8d7a7a0629
junair <cienciadedadosjunair@gmail.com>
2025-12-02 19:11:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
 
